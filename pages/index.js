29d50d0e783f16892bbe129aadee2aa1d0491837
Tuğba Koç <ktugbaa7@gmail.com>
2022-03-02 15:03:21 +0300

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,193 +1,120 @@
 import Head from 'next/head'
+import { useState } from 'react';
+
 
 export default function Home() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  const handleChange = (e) => {
+    e.preventDefault()
+    setTodo(e.target.value)
+  }
+  const removeTodo = (todo) => {
+    setTodos(todos.filter(t => t != todo))
+  }
+  
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1>To Do List</h1>
+      <div className="input-text">
+        <input className="input" value={todo} onChange={handleChange} placeholder=" Add a task..." />
+        <button onClick={() => setTodos([...todos, todo])}>Add</button>
+      </div>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+      {todos.map((todo) => (
+        <div className="inputbox">
+          <div className="todot">
+            <input type="checkbox" />
+            <p>{todo}</p>
+          </div>
+          <div>
+          
+          </div>
+          <div className="bars">
+            <ul>
+              <button>Pin on the top</button>
+              <button>Add a memo</button>
+              <button className="remove" onClick={() => removeTodo(todo)}>Delete</button>
+            </ul>
+          </div>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
-      </main>
+      ))}
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
+        .bars ul{
+          display: none;
+
+        }
         .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
+          width: 420px;
+          height: 540px;
+          background-color: #3C424A;
+          border-radius: 5px;
+        }
+        .input-text{
           display: flex;
-          flex-direction: column;
+          justify-content: center;
+          margin-top: 20px;
+        }
+        .input{
+          width:70%;
+          line-height: 30px;
+          background-color: #343A40;
+          border: none;
+          color: #EEEEEE;
+          border-radius: 5px;
+          border: 1px solid #2E3238;
+        }
+        h1{
+          border-bottom: 1px solid #343940;
+          color: #EEEEEE;
+          display: flex;
+          justify-content: center;
+          font-size: 16px;
+          padding: 7px 0 7px;
+          font-weight: normal;
+        }
+        .inputbox{
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          width: 80%;
+          margin: auto;
+          height: 40px;
+        }
+        .todot{
+          display: flex;
           justify-content: center;
           align-items: center;
         }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        p{
+          color: #EEEEEE;
+          margin-left: 5px;
         }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        .remove{
+          width: 50px;
+          height: 20px;
+          background-color: #484F59;
+          border: #383D45;
+          color: #A9B1BA;
         }
-
-        footer img {
-          margin-left: 0.5rem;
+        input[type="checkbox"]{
+          appearance: none;
+          height: 17px;
+          width: 17px;
+          background-color: #484F59;
+          border: 1px solid #2E3238;
+          border-radius: 2px;
+          cursor: pointer;
         }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
+        .bars{
+          
           text-align: center;
         }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
+        
       `}</style>
 
       <style jsx global>{`
@@ -199,7 +126,15 @@ export default function Home() {
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
-
+        body{
+          background-color: #F06292;
+          position: relative;
+          width: 100%;
+          height: 700px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
         * {
           box-sizing: border-box;
         }
